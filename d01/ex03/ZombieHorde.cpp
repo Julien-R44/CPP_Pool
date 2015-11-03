4216f1a7ea1928686cd847326f49ff3f8960a77f
Julien RIPOUTEAU <jripoute@e1r10p1.42.fr>
2015-11-03 09:11:14 +0100

    delete unused variables

--- d01/ex03/ZombieHorde.cpp
+++ d01/ex03/ZombieHorde.cpp
@@ -6,20 +6,13 @@
 /*   By: y0ja <y0ja@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/31 22:05:48 by y0ja              #+#    #+#             */
-/*   Updated: 2015/10/31 22:39:15 by y0ja             ###   ########.fr       */
+/*   Updated: 2015/11/03 09:09:13 by jripoute         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ZombieHorde.hpp"
 
-const char g_randomNames[] =
-	"0123456789"
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	"abcdefghijklmnopqrstuvwxyz";
-
 ZombieHorde::ZombieHorde( int n ) {
-	int i = 0;
-
 	this->_hordeArray = new Zombie[n];
 	this->_hordeArrayLen = n;
 	return ;
