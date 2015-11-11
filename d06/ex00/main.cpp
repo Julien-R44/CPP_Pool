4478155e73f7a3d9f863bfa897611eb382dc5b01
y0ja <jripoute@student.42.fr>
2015-11-11 20:02:03 +0100

    convert.cpp

--- d06/ex00/main.cpp
+++ d06/ex00/main.cpp
@@ -1,86 +1,59 @@
-#include <sstream>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: y0ja <y0ja@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/10 06:48:10 by y0ja              #+#    #+#             */
+/*   Updated: 2015/11/11 04:42:28 by y0ja             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
-#include <string>
-#include <cmath>
+#include <cstdlib>
+#include "Convert.hpp"
 
-double string_to_double( const std::string& s )
-{
-	std::istringstream i(s);
-	double x;
-	if (!(i >> x))
-		return 0;
-	return x;
-}
-
-int main(int argc, char const *argv[])
-{
-	if (argc < 2){
-		std::cout << "Must have a string as argument." << std::endl;
-		return 0;
-	}
-	int convInt = 0;
-	float convFloat = 0.0f;
-	double convDouble = 0.0;
-	char convChar = '\0';
-	bool converted = false;
-
-	const std::string str = argv[1];
-	try{
-		convFloat = std::stof(str);
-		if (str.find("f") != std::string::npos){
-			convInt = static_cast<int>(convFloat);
-			convDouble = static_cast<double>(convFloat);
-			convChar = static_cast<char>(convFloat);
-			std::cout << "Convert from float" << std::endl;
-			converted = true;
-		}
-	}catch(std::exception& e){
-	}
-	if (!converted){
-		try{
-			convDouble = std::stod(str);
-			convInt = static_cast<int>(convDouble);
-			convChar = static_cast<char>(convDouble);
-			convDouble = static_cast<double>(convDouble);
-			std::cout << "Convert from double" << std::endl;
-			converted = true;
-		}catch(std::exception& e){
-		}
+int main(int ac, char const *av[]) {
+	if (ac != 2) {
+		std::cout << "usage: ./convert <value>" << std::endl;
+		return (1);
 	}
 
-	if (!converted){
-		if (std::istringstream(str) >> convInt){
-			std::cout << "Convert from int" << std::endl;
-			converted = true;
-			convFloat = static_cast<float>(convInt);
-			convDouble = static_cast<double>(convInt);
-			convChar = static_cast<char>(convInt);
-			converted = true;
-		}
-		else if (str == "nan"){
-			std::cout << "char: impossible" << std::endl;
-			std::cout << "int: impossible" << std::endl;
-			std::cout << "float: nanf" << std::endl;
-			std::cout << "double: nanf" << std::endl;
-		}
-		else{
-			convChar = argv[1][0];
-			std::cout << "Convert from char" << std::endl;
-			converted = true;
-			convFloat = static_cast<float>(convChar);
-			convDouble = static_cast<double>(convChar);
-			convInt = static_cast<int>(convChar);
-			converted = true;
-		}
+	Convert		converter(av[1]);
+
+	try {
+		std::cout << "Char : ";
+		char c = converter;
+		std::cout << '\'' << c << '\'' << std::endl;
+	} catch (Convert::NotDisplayable & e) {
+		std::cout << "Non displayable" << std::endl;
+	} catch (std::exception & e ) {
+		std::cout << "impossible" << std::endl;
 	}
-	if (std::isnan(convFloat) || std::isinf(convFloat)){
-		std::cout << "int: impossible" << std::endl;
-		std::cout << "char: impossible" << std::endl;
-	}else{
-		std::cout << "int: " << convInt << std::endl;
-		std::cout << "char: " << convChar << std::endl;
+
+	try {
+		std::cout << "Int : ";
+		int n = converter;
+		std::cout << n << std::endl;
+	} catch (std::exception & e) {
+		std::cout << "impossible" << std::endl;
 	}
-	std::cout << "float: " << convFloat << "f" << std::endl;
-	std::cout << "double: " << convDouble << std::endl;
+
+	try {
+		std::cout << "Float : ";
+		float n = converter;
+		std::cout << n << std::endl;
+	} catch (std::exception & e) {
+		std::cout << "impossible" << std::endl;
+	}
+
+	try {
+		std::cout << "Double : ";
+		double n = converter;
+		std::cout << n << std::endl;
+	} catch ( std::exception & e ) {
+		std::cout << "impossible" << std::endl;
+	}
 	return 0;
 }
